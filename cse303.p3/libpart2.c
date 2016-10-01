7cac90724b780f1e01939c342f986df4298b0f5a
Billy <wild188@gmail.com>
2016-10-01 14:43:54 -0400

    made signatures for liabpart2

--- cse303.p3/libpart2.c
+++ cse303.p3/libpart2.c
@@ -1,4 +1,6 @@
 #include <stdint.h>
+#include <stddef.h>
+#include <stdio.h>
 
 /*
  * We aren't providing much code here.  You'll need to implement your own
@@ -16,3 +18,27 @@ int map_count();
 void map_dump();
 
 /* TODO: Your code goes here */
+
+int unfreeBlocks; //To count allocations and frees
+
+//Declaring and mapping constructor and deconstructor functions
+void OpenMemoryManagement()__attribute__((constructor)); 
+void CloseMemoryManagement()__attribute__((destructor));
+
+void OpenMemoryManagement() { // This is the constructor
+    unfreeBlocks = 0;
+}
+
+void CloseMemoryManagement(){ // This is the deconstructor
+    printf("%i unfreed blocks of memory.\n", unfreeBlocks);
+}
+
+void * malloc(size_t size){
+    unfreeBlocks++;
+    //call real malloc
+}
+
+void free(void * space){
+    unfreeBlocks--;
+    //call real free
+}
