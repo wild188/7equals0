b2b27a27811b87de092516b1c2fa39513dcc21e4
Billy <wild188@gmail.com>
2016-10-08 00:24:46 -0400

    nate is smaller than fergus

--- cse303.p3/libpart4.c
+++ cse303.p3/libpart4.c
@@ -6,18 +6,21 @@
 #include <execinfo.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
 
 void CheckPipe()__attribute__((constructor));
 
 void CheckPipe(){
-    void **calls
+    void **calls;
     int size = 100;
     int ret = backtrace(calls, size);
     int i;
     while(calls[i] != NULL){
-        if(strstr(calls[i], 'printf') || strstr(calls[i], 'scanf'){
+        if(strstr(calls[i], 'printf') || strstr(calls[i], 'scanf')){
             int pageSize = sysconf(_SC_PAGE_SIZE);
-            mprotect((void*)((size_t)&pageSize) & (pageSize - 1), pageSize, PROT_NONE);
+            mprotect((void*)(((size_t)&pageSize) & (pageSize - 1)), pageSize, PROT_NONE);
         }
         i++;
     } 
