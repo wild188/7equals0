c585eb0f576be8d2131929268e4146320d8801d7
nateg10 <npg216@lehigh.edu>
2016-10-08 00:16:34 -0400

    part 4

--- cse303.p3/libpart4.c
+++ cse303.p3/libpart4.c
@@ -1,6 +1,25 @@
-/*
- * We aren't providing much code here.  You'll need to implement your own
- * dlsym(), as well as any constructors or destructors for your library.
- */
+#include <stdint.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <dlfcn.h>
+//#include "list.c"
+#include <execinfo.h>
+#include <string.h>
+#include <stdlib.h>
 
-/* TODO: Your code goes here */
+void CheckPipe()__attribute__((constructor));
+
+void CheckPipe(){
+    void **calls
+    int size = 100;
+    int ret = backtrace(calls, size);
+    int i;
+    while(calls[i] != NULL){
+        if(strstr(calls[i], 'printf') || strstr(calls[i], 'scanf'){
+            int pageSize = sysconf(_SC_PAGE_SIZE);
+            mprotect((void*)((size_t)&pageSize) & (pageSize - 1), pageSize, PROT_NONE);
+        }
+        i++;
+    } 
+
+}
